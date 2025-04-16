ffc1b84aca1c562071a6e85f4ce959e3a0f2f929
NewJKH <newsun0805@gmail.com>
2025-04-16 15:20:11 +0900

    first commit

--- src/main/java/org/calculator/level1/App.java
+++ src/main/java/org/calculator/level1/App.java
@@ -18,15 +18,45 @@ public class App {
     public static void main(String[] args) {
         while (true){
             Scanner scanner = new Scanner(System.in);
+            // 첫번째 입력...
+            System.out.print(" 첫번째 정수를 입력하세요: ");
+            String val1 = scanner.next();
 
+            if( val1.equals("exit")) {
+                System.out.println(" 계산기를 종료 합니다. ");
+                return;
+            }
+            // 두번째 입력...
+            System.out.print(" 두번째 정수를 입력하세요: ");
+            String val2 = scanner.next();
 
+            if( val2.equals("exit")) {
+                System.out.println(" 계산기를 종료 합니다. ");
+                return;
+            }
+            // 여기서부터... 글자를 숫자로 변환.... 예..외 처리 하...고 싶다..
+            int num = Integer.parseInt(val1);
+            int num2 = Integer.parseInt(val2);
 
+            System.out.print(" 기호(➕,➖,✖\uFE0F,➗) 선택: ");
+            char symbol = scanner.next().charAt(0);
 
+            if ( num2 <= 0 && symbol == '/'){
+                System.out.println(" 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                continue;
+            }
 
+            String result;
+            switch (symbol){
+                case '+'-> result = String.valueOf(num + num2);
+                case '-'-> result = String.valueOf(num - num2);
+                case '/'-> result = String.valueOf(num / num2);
+                case '*'-> result = String.valueOf(num * num2);
+                default-> result = " 기호가 잘못되었습니다. ";
+            }
+            System.out.println(" 결과 "+result);
 
-
-
-
+            
         }
     }
 }
