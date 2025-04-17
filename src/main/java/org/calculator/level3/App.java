f3b9df6236e761b03267945e2a6fdf46a4a67c3f
NewJKH <newsun0805@gmail.com>
2025-04-17 13:14:32 +0900

    level 3

--- src/main/java/org/calculator/level3/App.java
+++ src/main/java/org/calculator/level3/App.java
@@ -10,22 +10,22 @@ import java.util.Scanner;
 
 public class App {
     /*
-     * [app.App]                   메인 시작점
-     *     |
-     *     v
-     * [service.ScannerService]    사용자 입력 처리 (Scanner 기반)
-     *     |
-     *     v
-     *     |---> [util.InputParser]    입력 값 파싱 (숫자, 연산자)
-     *     |
-     *     v
-     * [service.CalculationService]    계산 흐름 총괄 (계산 + 결과 저장/조회)
-     *     |
-     *     |---> [model.ArithmeticCalculator]   실제 사칙연산 수행
-     *     |
-     *     |---> [model.Result]                 연산 결과 객체 생성
-     *     |
-     *     |---> [repository.ResultRepository]  결과 저장 및 조회
+         * [app.App]                   메인 시작점
+         *     |
+         *     v
+         * [service.ScannerService]    사용자 입력 처리 (Scanner 기반)
+         *     |
+         *     v
+         *     |---> [util.InputParser]    입력 값 파싱 (숫자, 연산자)
+         *     |
+         *     v
+         * [service.CalculationService]    계산 흐름 총괄 (계산 + 결과 저장/조회)
+         *     |
+         *     |---> [model.ArithmeticCalculator]   실제 사칙연산 수행
+         *     |
+         *     |---> [model.Result]                 연산 결과 객체 생성
+         *     |
+         *     |---> [repository.ResultRepository]  결과 저장 및 조회
      */
 
     public static void main(String[] args) {
@@ -34,7 +34,6 @@ public class App {
         ScannerService scannerService = new ScannerService(new Scanner(System.in));
         CalculationService calculationService = new CalculationService();
         ResultService resultService = new ResultService(resultRepository);
-
         do {
             try {
                 Number val1 = scannerService.getNumber(" 첫번째 숫자를 입력해주세요: ");
