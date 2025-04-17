5ccb9b0eeec7df51f476ef2fe975a43e23e0fc31
NewJKH <newsun0805@gmail.com>
2025-04-17 10:49:34 +0900

    level3_arithmetic_차트 변경

--- src/main/java/org/calculator/level3/App.java
+++ src/main/java/org/calculator/level3/App.java
@@ -7,26 +7,27 @@ import java.util.Scanner;
 
 public class App {
     /*
-     * 플로우차트
-     * [app.App]                       메인 시작점
-        |
-        v
-    [util.InputParser]             사용자 입력 파싱 (숫자 + 연산자)
-        |
-        v
-    [service.CalculationService]   계산 흐름 관리 (계산 + 결과 저장/조회)
-        |
-        |---> [calculator.ArithmeticCalculator]   실제 계산 수행
-        |
-        |---> [model.Result]                      계산 결과 객체 생성
-        |
-        |---> [repository.ResultRepository]       결과 저장 및 조회
-        |
+     * [app.App]                   메인 시작점
+     *     |
+     *     v
+     * [service.ScannerService]    사용자 입력 처리 (Scanner 기반)
+     *     |
+     *     v
+     *     |---> [util.InputParser]    입력 값 파싱 (숫자, 연산자)
+     *     |
+     *     v
+     * [service.CalculationService]    계산 흐름 총괄 (계산 + 결과 저장/조회)
+     *     |
+     *     |---> [model.ArithmeticCalculator]   실제 사칙연산 수행
+     *     |
+     *     |---> [model.Result]                 연산 결과 객체 생성
+     *     |
+     *     |---> [repository.ResultRepository]  결과 저장 및 조회
      */
+
     public static void main(String[] args) {
         ScannerService scannerService = new ScannerService(new Scanner(System.in));
         CalculationService calculationService = new CalculationService();
-
         do {
 
 
--- src/main/java/org/calculator/level3/service/ScannerService.java
+++ src/main/java/org/calculator/level3/service/ScannerService.java
@@ -1,11 +1,16 @@
 package org.calculator.level3.service;
 
+import org.calculator.level3.util.InputParser;
+
 import java.util.Scanner;
 
 public class ScannerService {
     private final Scanner scanner;
+    private final InputParser inputParser;
+
     public ScannerService(Scanner scanner) {
         this.scanner = scanner;
-
+        this.inputParser = new InputParser();
     }
+
 }
