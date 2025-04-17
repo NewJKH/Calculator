813406e0aed58cef2e4db66720f8f26350d5bba6
NewJKH <newsun0805@gmail.com>
2025-04-17 10:30:13 +0900

    level3_arithmetic_유연하게 수정2

--- src/main/java/org/calculator/level3/util/InputParser.java
+++ src/main/java/org/calculator/level3/util/InputParser.java
@@ -1,7 +1,5 @@
 package org.calculator.level3.util;
 
-import java.util.Objects;
-
 public class InputParser {
     /*
     여기서는 입력값 ( 1번째 값 , (기호) , ( 2번째 값 ) 으로 받기
@@ -16,9 +14,15 @@ public class InputParser {
 
     /**
      * @param obj 일단 모든 값을 String 이든 Number 이든 다 받게 설정하고 안에서 파싱
+     *            근데 어차피.... Scanner 에서는 String 이라면
      * @return Number 또는 에외
      */
-    public Number parseNumber(Objects obj){
-        return ;
+    public Number parseNumber(Object obj) {
+        if (obj instanceof Number number) return number;
+        if (obj instanceof String str)
+            try { return Double.parseDouble(str); }
+            catch (NumberFormatException ignored) {  }
+
+        throw new IllegalArgumentException("숫자 타입이 아닙니다.");
     }
 }
