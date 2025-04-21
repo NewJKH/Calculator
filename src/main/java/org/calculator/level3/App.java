3fe4902d582724924fd4aab41214e816dfef3eb3
NewJKH <newsun0805@gmail.com>
2025-04-21 16:47:53 +0900

    Level 3 정리

--- src/main/java/org/calculator/level3/App.java
+++ src/main/java/org/calculator/level3/App.java
@@ -9,24 +9,6 @@ import org.calculator.level3.service.ScannerService;
 import java.util.Scanner;
 
 public class App {
-    /*
-         * [app.App]                   메인 시작점
-         *     |
-         *     v
-         * [service.ScannerService]    사용자 입력 처리 (Scanner 기반)
-         *     |
-         *     v
-         *     |---> [util.InputParser]    입력 값 파싱 (숫자, 연산자)
-         *     |
-         *     v
-         * [service.CalculationService]    계산 흐름 총괄 (계산 + 결과 저장/조회)
-         *     |
-         *     |---> [model.ArithmeticCalculator]   실제 사칙연산 수행
-         *     |
-         *     |---> [model.Result]                 연산 결과 객체 생성
-         *     |
-         *     |---> [repository.ResultRepository]  결과 저장 및 조회
-     */
 
     public static void main(String[] args) {
         ResultRepository resultRepository = new ResultRepository();
